d79153c28ed0169439ab82d81b37d334d3007287
shrikar007 <shrikarvaitala90@gmail.com>
2020-06-08 17:08:57 +0530

    monitoring in golang

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module useofchannels
 go 1.14
 
 require (
+	github.com/gorilla/mux v1.7.4
 	github.com/jinzhu/gorm v1.9.12
 	gopkg.in/robfig/cron.v2 v2.0.0-20150107220207-be2e0b0deed5
 )
--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"gopkg.in/robfig/cron.v2"
 	"log"
 	"net/http"
+	"net/http/pprof"
 	"runtime"
 	"sync"
+	"time"
 )
 type Profile struct {
 	Id     int       `gorm:"unique;not null;PRIMARY_KEY;AUTO_INCREMENT"`
@@ -21,21 +24,31 @@ var Flag bool
 
 func main() {
 	go memorycheck()
-	http.HandleFunc("/", rungoroutine)
-	http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", rungoroutine)
+
+	r.HandleFunc("/debug/pprof/", pprof.Index)
+	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	r.Handle("/debug/pprof/heap", pprof.Handler("heap"))
+	r.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
+	r.Handle("/debug/pprof/block", pprof.Handler("block"))
+	r.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
+	http.ListenAndServe(":8080",r )
 }
 
 func rungoroutine(w http.ResponseWriter, r *http.Request) {
 	var wg sync.WaitGroup
-	off:=0
-	Pro:=Profiles{}
-	var data=make(chan interface{},10)
+
 	db, err := gorm.Open("mysql", "root:root@tcp(192.168.43.106:3306)/channels")
 	if err != nil {
 		log.Fatal(err)
 	}
 	db.AutoMigrate(&Profile{})
-
+	off:=0
+	Pro:=Profiles{}
+	data:=make(chan Profiles,10)
 	for i:=0;i<=10;i++  {
 		wg.Add(1)
 		go getdata(&wg,db,off,data,Pro)
@@ -46,13 +59,18 @@ func rungoroutine(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 	wg.Wait()
-	for i:=0;i<len(data);i++{
+
+	//for x:=range data{
+	//	fmt.Println(x)
+	//}
+
+	for j:=0;j<=len(data)+1;j++{
 		fmt.Println(<-data)
 	}
 	fmt.Fprintf(w,"Done")
 }
 
-func getdata(wg *sync.WaitGroup,db *gorm.DB, off int,d chan interface{},pro Profiles){
+func getdata(wg *sync.WaitGroup,db *gorm.DB, off int,d chan Profiles,pro Profiles){
 	defer wg.Done()
 	db.Limit(5).Offset(off).Find(&pro)
 	if len(pro)==0{
@@ -60,6 +78,7 @@ func getdata(wg *sync.WaitGroup,db *gorm.DB, off int,d chan interface{},pro Prof
 		return
 	}
 	d<-pro
+	time.Sleep(time.Second)
 }
 func memorycheck(){
 	c := cron.New()
